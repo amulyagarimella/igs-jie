f94d00ab0767178dc78036a5360222604fc2b897
amulyagarimella <amu.garimella@gmail.com>
2023-04-11 11:14:23 -0400

    memory

--- demo01/memory.py
+++ demo01/memory.py
@@ -1,66 +1,97 @@
 import pickle
+import glob
+import pandas as pd
+from collections import Counter
+import math
+import numpy as np
+from scipy.stats.stats import pearsonr   
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-with open('seqfishE14_demo01_res_chrom_all.pkl', 'rb') as f:
-    chr_fibers = pickle.load(f)
+## compile fiber data across cells and chr
 
+cell_chr_fiber = pd.DataFrame(columns=["cell", "FOV", "chrnum", "x_hat", "y_hat", "z_hat", "fiber"])
 
-## assign fiber numbers for all cells
-
-for 
+available_chr = glob.glob('seqfishE14_demo01_res_chr*')
+for chr_file in available_chr[0:2]:
+    with open(chr_file, 'rb') as f:
+        chr_fibers = pickle.load(f)
+        for i in range(len(chr_fibers)):
+            # for each cell
+            all_cell_fibers = chr_fibers[i]
+            for c in range(len(all_cell_fibers)):
+                cell_fibers = all_cell_fibers[c]
+                cell_fibers['chrnum'] = cell_fibers['chr'].str.replace("chr","").astype(int)
+                # append, cell, fov, chrom, median pos
+                med_info = cell_fibers.loc[:,['cell','FOV','chrnum','x_hat','y_hat','z_hat']].median(numeric_only=None)
+                # assign fiber number
+                med_info['fiber'] = c
+                cell_chr_fiber = pd.concat([cell_chr_fiber,pd.DataFrame(med_info).T],axis=0)
+            # print(cell_chr_fiber)
 
 ## create #chr x #chr mean spatial distance matrix for all cells
 
-# find mean 3-pt location for each fiber for each chr; store this
+cells = list(Counter(cell_chr_fiber['cell']).keys())
+chrs = list(Counter(cell_chr_fiber['chrnum']).keys())
 
-# for chr i
-    # for chr j ≠ i
-        # for each fiber of chr i
-            # for all fibers of chr j
-                # determine dist between two fibers; add to list
-        # mean of list: distance between chr i <-> j
+# initialize matrix
+chr_vs_chr = []
 
-
-
-
-
-
-"""
-corr_mat = zeros(num_cells,num_cells);
- 
-for i=1:num_cells
-    for j=1:num_cells
-        if i>j
-            corr_mat(i,j) = corr(reshape(chr_dist_mat(i,:,:),[],1),reshape(chr_dist_mat(j,:,:),[],1));
-        end
-    end
-end
- 
-
-"""
+# for each cell
+for c in cells:
+    # for chr i
+    cvc = np.empty([len(chrs), len(chrs)])
+    for i in range(len(chrs)):
+        # for chr j ≠ i
+        for j in range(len(chrs)):
+            if j != i:
+                dists = []
+                fibers_i = cell_chr_fiber.loc[(cell_chr_fiber.cell==int(c)) & (cell_chr_fiber.chrnum==int(chrs[i]))]
+                f_i_pts = fibers_i.loc[fibers_i.fiber==int(i),['x_hat', 'y_hat','z_hat']].values
+                fibers_j = cell_chr_fiber.loc[(cell_chr_fiber.cell==int(c)) & (cell_chr_fiber.chrnum==int(chrs[j]))]
+                f_j_pts = fibers_j.loc[fibers_j.fiber==int(j),['x_hat', 'y_hat','z_hat']].values
+                for f_i_pt in f_i_pts:
+                    for f_j_pt in f_j_pts:
+                        d = math.dist(f_i_pt,f_j_pt)
+                        dists.append(d)
+                cvc[i][j] = np.mean(dists)
+            else:
+                pass
+    chr_vs_chr.append(cvc)
 
 # create correlation matrix; # cells x # cells
 
-"""
-% creates a matrix where (i,j) = 1 indicates that cell i and j were from the same fov
-same_fov = double(repmat(cell_list(:,1),1,num_cells) == repmat(cell_list(:,1),1,num_cells)')
- 
-same_fov_corr = corr_mat(find(same_fov));
-same_fov_corr = same_fov_corr(same_fov_corr~=0);
- 
-diff_fov_corr = corr_mat(find(~same_fov));
-diff_fov_corr = diff_fov_corr(diff_fov_corr~=0);
- 
-figure;
-ksdensity(same_fov_corr); hold on;
-ksdensity(diff_fov_corr); hold on;
- 
-figure;
-histogram(same_fov_corr,-1:0.1:1); hold on;
-histogram(diff_fov_corr,-1:0.1:1); hold on;
-xlim([-1 1])
+cell_v_cell = np.empty([len(cells), len(cells), 2])
 
-"""
+same_fov = []
+diff_fov = [] 
+for i in range(len(cells)):
+    for j in range(i,len(cells)):
+        try:
+            r = pearsonr(chr_vs_chr[i].flatten(), chr_vs_chr[j].flatten())[0]
+        except:
+            pass
+        cell_v_cell[i][j][0] = r
+        cell_v_cell[j][i][0] = r
+        if list(cell_chr_fiber.loc[cell_chr_fiber.cell==cells[i], "FOV"])[0] == list(cell_chr_fiber.loc[cell_chr_fiber.cell==cells[j], "FOV"])[0]:
+            same_fov.append(r)
+            cell_v_cell[i][j][1] = 1
+            cell_v_cell[j][i][1] = 1
+        else:
+            diff_fov.append(r)
+            cell_v_cell[i][j][1] = 0
+            cell_v_cell[j][i][1] = 0
 
+# create histograms of fov-based relationships: TODO
+
+print(len(same_fov))
+
+sns.histplot(x=same_fov)
+sns.histplot(x=diff_fov)
+plt.xlim((-1,1))
+plt.show()
+
+# plot for fov 3: TODO
 """
 
 fov = 3;
@@ -89,4 +120,4 @@ end
 """
 
 
-# PCA the corr matrix 
+# PCA the corr matrix: TODO
