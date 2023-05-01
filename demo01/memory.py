e5e3bd61988f5e32b60c571dfb0a944dad869d0d
amulyagarimella <amu.garimella@gmail.com>
2023-05-01 16:38:46 -0400

    save med coords

--- demo01/memory.py
+++ demo01/memory.py
@@ -7,12 +7,16 @@ import numpy as np
 from scipy.stats import pearsonr   
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
+np.seterr(all='print')
+
 
 ## compile fiber data across cells and chr
 
 cell_chr_fiber = pd.DataFrame(columns=["cell", "FOV", "chrnum", "x_hat", "y_hat", "z_hat", "fiber"])
 
 available_chr = glob.glob('seqfishE14_demo01_res_chr*')
+# available_chr = [f'seqfishE14_demo01_res_chr{n}.pkl' for n in [1,2,3,10,11,12,13]]
 for chr_file in available_chr:
     #with open(chr_file, 'rb') as f:
         # ModuleNotFoundError: No module named 'pandas.core.indexes.numeric'
@@ -21,48 +25,72 @@ for chr_file in available_chr:
     for i in range(len(chr_fibers)):
         # for each cell
         all_cell_fibers = chr_fibers[i]
+        # print(all_cell_fibers)
         for c in range(len(all_cell_fibers)):
-            cell_fibers = all_cell_fibers[c]
-            cell_fibers['chrnum'] = cell_fibers['chr'].str.replace("chr","")
+            cell_fibers = all_cell_fibers[c].loc[:,['finalcellID','FOV','hyb','x_hat','y_hat','z_hat', 'chr']]
+            cell_fibers['chrnum'] = cell_fibers['chr'].str.replace("chr","").astype(float)
             # append, cell, fov, chrom, median pos
+            #if (i >= 28):
+                #print(cell_fibers)
+                #print(cell_fibers.median(numeric_only=True))
             try:
-                med_info = cell_fibers.loc[:,['cell','FOV','chrnum','x_hat','y_hat','z_hat']].median(numeric_only=None)
+                med_info = cell_fibers.sort_values(by='hyb').median(numeric_only=True)
             except:
-                continue
+                #print(cell_fibers)
+                raise
             # assign fiber number
+            # do chr num a better way TODO
             med_info['fiber'] = c
+            # bin chr into chunks
             cell_chr_fiber = pd.concat([cell_chr_fiber,pd.DataFrame(med_info).T],axis=0)
         # print(cell_chr_fiber)
+    cell_chr_fiber.to_csv("cell_chr_fiber_med.csv")
     print("completed")
 
-## create #chr x #chr mean spatial distance matrix for all cells
+cell_chr_fiber = pd.read_csv("cell_chr_fiber_med.csv")
 
-cells = list(Counter(cell_chr_fiber['cell']).keys())
+## create #chr x #chr mean spatial distance matrix for all cells
+cells = list(Counter(cell_chr_fiber['finalcellID']).keys())
 chrs = list(Counter(cell_chr_fiber['chrnum']).keys())
 
+print(cells)
+print(chrs)
 # initialize matrix
 chr_vs_chr = []
+
+#todo use set insted of counter
 
 # for each cell
 for c in cells:
     # for chr i
     cvc = np.empty([len(chrs), len(chrs)])
+    cvc[:] = np.nan
     for i in range(len(chrs)):
         # for chr j != i
-        for j in range(len(chrs)):
+        for j in range(i+1,len(chrs)):
             if j == i:
                 continue
             dists = []
-            fibers_i = cell_chr_fiber.loc[(cell_chr_fiber.cell==int(c)) & (cell_chr_fiber.chrnum==int(chrs[i]))]
+            fibers_i = cell_chr_fiber.loc[(cell_chr_fiber.finalcellID==int(c)) & (cell_chr_fiber.chrnum==int(chrs[i]))]
             f_i_pts = fibers_i.loc[fibers_i.fiber==int(i),['x_hat', 'y_hat','z_hat']].values
-            fibers_j = cell_chr_fiber.loc[(cell_chr_fiber.cell==int(c)) & (cell_chr_fiber.chrnum==int(chrs[j]))]
+            #print(f_i_pts)
+            fibers_j = cell_chr_fiber.loc[(cell_chr_fiber.finalcellID==int(c)) & (cell_chr_fiber.chrnum==int(chrs[j]))]
             f_j_pts = fibers_j.loc[fibers_j.fiber==int(j),['x_hat', 'y_hat','z_hat']].values
+            #print(f_j_pts)
+            if len(f_i_pts) == 0 or len(f_j_pts) == 0:
+                continue
             for f_i_pt in f_i_pts:
                 for f_j_pt in f_j_pts:
                     d = math.dist(f_i_pt,f_j_pt)
                     dists.append(d)
-            cvc[i][j] = np.mean(dists)
+                    #print(d)
+            # take med
+            # or, 2 fibers close => merge
+            #print(dists)
+            cvc[i][j] = np.nanmean(dists)
     chr_vs_chr.append(cvc)
+    #print(cvc)
+    #print(chr_vs_chr)
 
 # create correlation matrix; # cells x # cells
 
@@ -71,16 +99,29 @@ cell_v_cell = np.empty([len(cells), len(cells), 2])
 same_fov = []
 diff_fov = [] 
 for i in range(len(cells)):
-    for j in range(i,len(cells)):
-        if i == j:
-            continue
+    for j in range(i+1,len(cells)):
         try:
-            r = pearsonr(chr_vs_chr[i].flatten(), chr_vs_chr[j].flatten())[0]
+            x = chr_vs_chr[i].flatten()
+            y = chr_vs_chr[j].flatten()
+            nas = np.logical_or(np.isnan(x), np.isnan(y))
+            r = pearsonr(x[~nas], y[~nas])[0]
+            if len(x[~nas]) < len(chrs) or len(y[~nas]) < len(chrs):
+                print("chr missing, skip")
+                continue
+            #print(f"({i},{j})")
+            if (r > .9):
+                print(f"({i},{j})")
+                print(x[~nas])
+                print(y[~nas])
         except:
+            # print("not able to calculate correlation")
             continue
+        if np.isnan(r):
+            continue
+        print(r)
         cell_v_cell[i][j][0] = r
         cell_v_cell[j][i][0] = r
-        if list(cell_chr_fiber.loc[cell_chr_fiber.cell==cells[i], "FOV"])[0] == list(cell_chr_fiber.loc[cell_chr_fiber.cell==cells[j], "FOV"])[0]:
+        if list(cell_chr_fiber.loc[cell_chr_fiber.finalcellID==cells[i], "FOV"])[0] == list(cell_chr_fiber.loc[cell_chr_fiber.finalcellID==cells[j], "FOV"])[0]:
             same_fov.append(r)
             cell_v_cell[i][j][1] = 1
             cell_v_cell[j][i][1] = 1
@@ -89,17 +130,25 @@ for i in range(len(cells)):
             cell_v_cell[i][j][1] = 0
             cell_v_cell[j][i][1] = 0
 
-# create histograms of fov-based relationships: TODO
+# create histograms of fov-based relationships
+
+print(f"# pairs with same fov: {len(same_fov)}")
+print(f"# pairs with diff fov: {len(diff_fov)}")
+
 
 ax = plt.gca()
 sns.kdeplot(x=same_fov,color="r",ax=ax)
 sns.kdeplot(x=diff_fov,color="b",ax=ax)
 plt.xlim((-1,1))
-plt.savefig("chr1_chr2_memviz.png")
-plt.savefig("chr1_chr2_memviz.svg")
+plt.savefig("allchr_memviz.png")
+plt.savefig("allchr_memviz.svg")
 plt.show()
 
+with open('same_fov_corr.pkl', 'wb') as f:
+    pickle.dump(same_fov, f)
 
+with open('diff_fov_corr.pkl', 'wb') as f:
+    pickle.dump(diff_fov, f)
 # plot for fov 3: TODO
 """
 
--- demo01/seqfish_viz.py
+++ demo01/seqfish_viz.py
@@ -4,85 +4,103 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
-def viz (fibers, chr_data, full_data, chr, fig, nrow, ncol, pos_col, idx, lines=True):
-    colors = ['r','b','g','c','m','y']
-    ax = fig.add_subplot(nrow,ncol,idx, projection='3d')
-    if len(fibers) == 0:
-        pass
-    for i in range(len(fibers)):
-        r = fibers[i].sort_values(by=pos_col,ascending=True)
-        ax.scatter(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'], c=colors[i])
-        if lines:
-            ax.plot(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'], c=colors[i])
-    # get data fw no match
-    clustered = pd.concat(fibers)
-    unclustered = pd.concat([clustered, chr_data], ignore_index=True)
-    unclustered = unclustered.drop_duplicates(keep=False)
-    # TODO adjust looks
-    ax.scatter(xs=unclustered['x_hat'], ys=unclustered['y_hat'], zs=unclustered['z_hat'], c=colors[(len(fibers) + 1) % len(colors)],alpha=.1)
-    rest_of_cell = pd.concat([clustered, full_data], ignore_index=True)
-    rest_of_cell = rest_of_cell.drop_duplicates(keep=False)
-    ax.scatter(xs=rest_of_cell['x_hat'], ys=rest_of_cell['y_hat'], zs=rest_of_cell['z_hat'], c='0.5',alpha=.01)
-    ax.set_title(f"{chr}")
 
-
+colors = ['r','g','g','c','m','y']
 ## selecting specific chr points (orig data)
-
-# unpickle chr_pts
-with open('seqfishE14_demo01_chr_pts.pkl', 'rb') as f:
-    chr_pts = pickle.load(f)
-
-chosen_celltype = 'mesc'
-fov = 0
-cell = 4
-# sel_chr_pts = chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['chr'] == chosen_chrom)]['data'].tolist()
-sel_pts = pd.concat(chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['FOV'] == fov) & (chr_pts['finalcellID'] == cell)]['data'].tolist())
-
-## selecting fibers
 available_chr = glob.glob('seqfishE14_demo01_res_chr*')
 available_chr_names = [x.split('.')[0].split('_')[-1] for x in available_chr]
 full_res = {}
 sel_res = {}
-print(available_chr_names)
-
+with open('seqfishE14_demo01_chr_pts.pkl', 'rb') as f:
+    chr_pts = pickle.load(f)
 for c in available_chr:
     with open(c, 'rb') as f:
         full_res[c.split('.')[0].split('_')[-1]] = pickle.load(f)
 
-"""for (key,item) in full_res.items():
-    print(item)"""
 
-# get fiber list for each chromosome
-for (c,fiber_list) in full_res.items():
-    # get all fibers across all cells
-    #print(fiber_list)
-    #print(len(fiber_list))
-    # select list of fibers that matches cell and FOV
-    #print(fiber_list[0:5])
-    for i in range(len(fiber_list)):
-        if len(fiber_list[i]) > 0:
-            print(c)
-            print(f"{int(list(fiber_list[i][0]['finalcellID'])[0])},{int(list(fiber_list[i][0]['FOV'])[0])}")
-            if int(list(fiber_list[i][0]["finalcellID"])[0]) == cell and int(list(fiber_list[i][0]["FOV"])[0]) == fov:
-                sel_res[c] = fiber_list[i]
+def viz (fibers, chr_data, full_data, chr, pos_col, ax, color, lines=True):
+    if len(fibers) == 0:
+        pass
+    r = pd.concat(fibers).sort_values(by=pos_col,ascending=True)
+    ax.scatter(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'], c=color)
+    # if lines:
+    #     ax.plot(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'])
+    """for i in range(len(fibers)):
+        r = fibers[i].sort_values(by=pos_col,ascending=True)
+        if color is None:
+            color = colors[i]
+        print(color)
+        ax.scatter(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'])
+        if lines:
+            ax.plot(xs=r['x_hat'], ys=r['y_hat'], zs=r['z_hat'])"""
+    # get data fw no match
+    # clustered = pd.concat(fibers)
+    # unclustered = pd.concat([clustered, chr_data], ignore_index=True)
+    # unclustered = unclustered.drop_duplicates(keep=False)
+    # TODO adjust looks
+    #if color is None:
+        #color = colors[(len(fibers) + 1) % len(colors)]
+    #ax.scatter(xs=unclustered['x_hat'], ys=unclustered['y_hat'], zs=unclustered['z_hat'], c=color,alpha=.1)
+    # rest_of_cell = pd.concat([clustered, full_data], ignore_index=True)
+    # rest_of_cell = rest_of_cell.drop_duplicates(keep=False)
+    #ax.scatter(xs=rest_of_cell['x_hat'], ys=rest_of_cell['y_hat'], zs=rest_of_cell['z_hat'], c='0.5',alpha=.01)
+    ax.set_title(f"{chr}")
+
+def plot_cell_chr (cell, axes, chrs_to_plot = None, color = None):
+    chosen_celltype = 'mesc'
+    # fov = 0
+    # sel_chr_pts = chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['chr'] == chosen_chrom)]['data'].tolist()
+    sel_pts = pd.concat(chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['finalcellID'] == cell)]['data'].tolist())
+    print(set(list(sel_pts.chr)))
+    ## selecting fibers
+    # get fiber list for each chromosome
+    for (c,fiber_list) in full_res.items():
+        if chrs_to_plot is not None:
+            if c not in chrs_to_plot:
+                continue
+        for i in range(len(fiber_list)):
+            if len(fiber_list[i]) > 0:
+                print(c)
+                # print(f"{int(list(fiber_list[i][0]['finalcellID'])[0])},{int(list(fiber_list[i][0]['FOV'])[0])}")
+                if int(list(fiber_list[i][0]["finalcellID"])[0]) == cell:
+                    sel_res[c] = fiber_list[i]
+                    break
+            else:
                 break
-        else:
-            break
+    ## plot fibers for each chr in sel_res
     
+    res_chr = list(sel_res.keys())
+    if chrs_to_plot is None:
+        size = int(np.ceil(np.sqrt(19)))
+        chrs_to_plot = res_chr
+    else:
+        size = int(np.ceil(np.sqrt(len(chrs_to_plot))))
 
-## plot fibers for each chr in sel_res
+    for c in chrs_to_plot:
+        print(c)
+        if chrs_to_plot is None:
+            try:
+                idx_to_plot = int(c.replace("chr",""))
+            except:
+                continue
+        else:
+            idx_to_plot = chrs_to_plot.index(c) + 1
+        sel_chr_pts = chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['chr'] == c) & (chr_pts['finalcellID'] == cell)]['data'].tolist()[0]
+        #  = int(c.replace("chr",""))
+        #print(sel_res[c])
+        print(color)
+        viz(sel_res[c], sel_chr_pts, sel_pts, c, 'hyb', axes[chrs_to_plot.index(c)], color=color)
+
 fig = plt.figure()
-res_chr = list(sel_res.keys())
-size = int(np.ceil(np.sqrt(len(res_chr))))
+ax1 = fig.add_subplot(1,2,1, projection='3d')
+ax2 = fig.add_subplot(1,2,2, projection='3d')
 
-for c in res_chr:
-    sel_chr_pts = chr_pts[(chr_pts['celltype'] == chosen_celltype) & (chr_pts['chr'] == c) & (chr_pts['FOV'] == fov) & (chr_pts['finalcellID'] == cell)]['data'].tolist()[0]
-    #  = int(c.replace("chr",""))
-    #print(sel_res[c])
-    viz(sel_res[c], sel_chr_pts, sel_pts, c, fig,size,size,'hyb',res_chr.index(c) +1)
+for c in [106]:
+    chrs_to_plot = ['chr7', 'chr12']
+    print(c % 2)
+    plot_cell_chr(c,(ax1, ax2), chrs_to_plot=chrs_to_plot, color=colors[c % 2])
 plt.tight_layout()
 plt.show()
-plt.clf()
 
 
 
